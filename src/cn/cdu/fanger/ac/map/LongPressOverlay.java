f04be298a1ba00c51ae53d7086ee51d1ea101711
cncduLee <shouli1990@gmail.com>
2013-04-03 17:04:16 +0800

    add current location to map view

---
+++ src/cn/cdu/fanger/ac/map/LongPressOverlay.java
@@ -0,0 +1,90 @@
+package cn.cdu.fanger.ac.map;
+
+import android.os.Handler;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnDoubleTapListener;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
+import cn.cdu.fanger.activity.MapMainActivity;
+
+
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+
+public class LongPressOverlay extends Overlay implements OnDoubleTapListener,OnGestureListener{
+
+	private MapMainActivity mContext;
+	private MapView mMapView;
+	private Handler mHandler;
+	private MapController mMapCtrl;
+	private GestureDetector gestureScanner = new GestureDetector(this);
+	private int level = 0;
+	
+	public LongPressOverlay(MapMainActivity context, MapView mapView, Handler handler,MapController mapCtrl){
+		mContext = context;
+		mMapView = mapView;
+		mHandler = handler;
+		mMapCtrl = mapCtrl;
+	}
+	
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event, MapView mapView) {
+		return gestureScanner.onTouchEvent(event);
+	}
+
+	@Override
+	public boolean onSingleTapConfirmed(MotionEvent e) {
+		return false;
+	}
+
+	@Override
+	public boolean onDoubleTap(MotionEvent e) {
+		return false;
+	}
+
+	@Override
+	public boolean onDoubleTapEvent(MotionEvent e) {
+		if(++level % 3 == 0){
+			mMapCtrl.zoomIn();
+			level = 0;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean onDown(MotionEvent e) {
+		return false;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent e) {
+		
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		return false;
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		return false;
+	}
+
+	@Override
+	public void onLongPress(MotionEvent e) {
+		mContext.locPoint = mMapView.getProjection().fromPixels((int) e.getX(),
+				(int) e.getY());
+		mHandler.sendEmptyMessage(1);
+	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		return false;
+	}
+
+}
---
+++ src/cn/cdu/fanger/ac/view/AbstractAsyncMapActivity.java
@@ -0,0 +1,53 @@
+package cn.cdu.fanger.ac.view;
+
+import com.google.android.maps.MapActivity;
+
+import android.app.ProgressDialog;
+
+public class AbstractAsyncMapActivity extends MapActivity implements AsyncActivity {
+
+	protected static final String TAG = AbstractAsyncActivity.class.getSimpleName();
+
+	private ProgressDialog progressDialog;
+
+	private boolean destroyed = false;
+
+	// ***************************************
+	// Activity methods
+	// ***************************************
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		this.destroyed = true;
+	}
+
+	// ***************************************
+	// Public methods
+	// ***************************************
+	public void showLoadingProgressDialog() {
+		this.showProgressDialog("Loading. Please wait...");
+	}
+
+	public void showProgressDialog(CharSequence message) {
+		if (this.progressDialog == null) {
+			this.progressDialog = new ProgressDialog(this);
+			this.progressDialog.setIndeterminate(true);
+		}
+
+		this.progressDialog.setMessage(message);
+		this.progressDialog.show();
+	}
+
+	public void dismissProgressDialog() {
+		if (this.progressDialog != null && !this.destroyed) {
+			this.progressDialog.dismiss();
+		}
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
